0a7d263a2b34ab2f6f5587a5a2042718c1980e7b
tuend-work <121396415+tuend-work@users.noreply.github.com>
2026-04-09 08:52:43 +0700

    .

--- ultimate-contact-button.php
+++ ultimate-contact-button.php
@@ -3,7 +3,7 @@
  * Plugin Name: Ultimate Contact Button
  * Plugin URI:  https://profiles.wordpress.org/nguyenductue/
  * Description: A professional contact button manager for Desktop and Mobile.
- * Version:     2.2.0
+ * Version:     2.2.1
  * Author:      Nguyễn Đức Tuệ
  * Author URI:  https://profiles.wordpress.org/nguyenductue/
  * License:     GPL-2.0+	
@@ -27,7 +27,7 @@ final class Ultimate_Contact_Button {
 	 *
 	 * @var string
 	 */
-	const VERSION = '2.2.0';
+	const VERSION = '2.2.1';
 
 	/**
 	 * Instance of this class
